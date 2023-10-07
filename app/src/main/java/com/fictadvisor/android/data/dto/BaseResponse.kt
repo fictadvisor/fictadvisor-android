42c423a738687de375d6a298745cffe7dd130315
vita133 <vitandrukhiv@gmail.com>
2023-10-07 14:50:05 +0300

    add BaseResponse class for handling response states

---
+++ app/src/main/java/com/fictadvisor/android/data/dto/BaseResponse.kt
@@ -0,0 +1,7 @@
+package com.fictadvisor.android.data.dto
+
+sealed class BaseResponse<out T> {
+    data class Success<out T>(val data: T? = null) : BaseResponse<T>()
+    data class Loading(val nothing: Nothing?=null) : BaseResponse<Nothing>()
+    data class Error(val msg: String?) : BaseResponse<Nothing>()
+}
