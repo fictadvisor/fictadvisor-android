4abc760f9784a24c45549fa5c015170cf0f95cd3
svitbar <svitbar1102@gmail.com>
2024-04-26 17:56:21 +0300

    Implement time and date parser

--- app/src/main/java/com/fictadvisor/android/ui/ScheduleFragment.kt
+++ app/src/main/java/com/fictadvisor/android/ui/ScheduleFragment.kt
@@ -13,15 +13,14 @@ import androidx.constraintlayout.widget.ConstraintSet
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.get
 import com.fictadvisor.android.R
 import com.fictadvisor.android.data.dto.BaseResponse
+import com.fictadvisor.android.data.dto.schedule.DetailedEventResponse
 import com.fictadvisor.android.data.dto.schedule.EventDTO
 import com.fictadvisor.android.data.dto.schedule.GetEventResponse
 import com.fictadvisor.android.data.dto.schedule.TDiscipline
 import com.fictadvisor.android.databinding.FragmentScheduleBinding
 import com.fictadvisor.android.repository.ScheduleRepository
-import com.fictadvisor.android.utils.StorageUtil
 import com.fictadvisor.android.viewmodel.ScheduleViewModel
 import com.fictadvisor.android.viewmodel.ScheduleViewModelFactory
 import kotlinx.coroutines.CoroutineScope
@@ -29,16 +28,17 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.time.LocalDateTime
-import java.time.ZoneId
-import java.time.ZonedDateTime
+import java.time.OffsetDateTime
 import java.time.format.DateTimeFormatter
 import java.util.Date
 import java.util.Locale
+import java.util.TimeZone
 
 class ScheduleFragment : Fragment() {
     private lateinit var binding: FragmentScheduleBinding
     private lateinit var scheduleViewModel: ScheduleViewModel
     private val scheduleRepository = ScheduleRepository()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -51,17 +51,14 @@ class ScheduleFragment : Fragment() {
         CoroutineScope(Dispatchers.IO).launch {
             //обережено хард код:
             scheduleViewModel.getEvents("2d6059e8-c625-4ce8-acf8-f865823f90d8", 11)
-            Log.d("yess", "1")
         }
 
         scheduleViewModel.getEventsResponse
             .observe(viewLifecycleOwner) { eventResponse ->
                 eventResponse?.let {
-                    Log.d("yess", "2")
                     handleScheduleResponse(eventResponse)
                 }
             }
-
 
         binding = FragmentScheduleBinding.inflate(inflater, container, false)
         val view = binding.root
@@ -74,10 +71,8 @@ class ScheduleFragment : Fragment() {
             is BaseResponse.Success -> {
                 val events = response.data?.events ?: emptyList()
                 val week = response.data?.week
-                Log.d("yess", "3"+response.data?.week)
 
-
-                drawUI(events)
+                drawUI(events, week)
             }
 
             is BaseResponse.Error -> {
@@ -88,8 +83,36 @@ class ScheduleFragment : Fragment() {
         }
     }
 
+    private fun getTime(string: String): String {
+        val defaultTimezone = TimeZone.getDefault().id
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX", Locale.UK)
+        val date = dateFormat.parse(string.replace("Z$", "+0000"))
 
-    private fun drawUI(events: List<EventDTO>) {
+        val offsetDateTime = OffsetDateTime.parse(dateFormat.format(date!!), DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSX"))
+
+        val timeFormatter = DateTimeFormatter.ofPattern("HH:mm")
+        val time = offsetDateTime.format(timeFormatter)
+
+        Log.d("TimeSchedule", "string: $string")
+        Log.d("TimeSchedule", "defaultTimezone: $defaultTimezone")
+        Log.d("TimeSchedule", "date: " + time)
+
+        return time
+    }
+
+    private fun getDate(string: String): String {
+        val offsetDateTime = OffsetDateTime.parse(string, DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSX"))
+
+        val dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+        val newDate = offsetDateTime.format(dateFormatter)
+
+        Log.d("TimeSchedule", "string: $string")
+        Log.d("TimeSchedule", "date: $newDate")
+
+        return newDate
+    }
+
+    private fun drawUI(events: List<EventDTO>, week: String?) {
         val timeColumn = binding.timeColumn
 
         for (i in 8 until 23) {
@@ -114,82 +137,110 @@ class ScheduleFragment : Fragment() {
         }
 
         val scheduleLayout = binding.scheduleLayout
+        val chosenDate = "2024-04-15"
 
         for (event in events) {
-            Log.d("ScheduleFragmentHey", "$event")
-            val cardView = layoutInflater.inflate(R.layout.card_view_template, null) as CardView
-            cardView.id = View.generateViewId()
+            if (getDate(event.startTime) == chosenDate) {
 
-            TODO("переписати, не працює")
-            var startTime = event.startTime
-            var endTime = event.endTime
-            val formatter2 = DateTimeFormatter.ofPattern("HH:mm")
-            val startTimeFormatted = startTime.format(formatter2)
-            val endTimeFormatted = endTime.format(formatter2)
+                Log.d("ScheduleFragmentHey", "$event")
+                val cardView = layoutInflater.inflate(R.layout.card_view_template, null) as CardView
+                cardView.id = View.generateViewId()
 
-            val layoutParams = ConstraintLayout.LayoutParams(
-                ConstraintLayout.LayoutParams.MATCH_CONSTRAINT,
-                dpToPx(calculateCardHeight(startTimeFormatted, endTimeFormatted))
-            )
-            cardView.layoutParams = layoutParams
+                val startTimeFormatted = getTime(event.startTime)
+                val endTimeFormatted = getTime(event.endTime)
 
-            val textViewSubject = cardView.findViewById<TextView>(R.id.subject)
-            textViewSubject.text = event.name
+                val layoutParams = ConstraintLayout.LayoutParams(
+                    ConstraintLayout.LayoutParams.MATCH_CONSTRAINT,
+                    dpToPx(calculateCardHeight(startTimeFormatted, endTimeFormatted))
+                )
+                cardView.layoutParams = layoutParams
 
-            val textViewTime = cardView.findViewById<TextView>(R.id.time)
-            textViewTime.text = "${event.startTime} - ${event.endTime}"
+                val textViewSubject = cardView.findViewById<TextView>(R.id.subject)
+                textViewSubject.text = event.name
 
-            val textViewTeacher = cardView.findViewById<TextView>(R.id.teacher)
-            // textViewTeacher.text = event.
+                val textViewTime = cardView.findViewById<TextView>(R.id.time)
+                textViewTime.text = "$startTimeFormatted - $endTimeFormatted"
 
-            val cardColor = when (event.disciplineType?.name) {
-                TDiscipline.LECTURE -> R.color.lecture_side_on
-                TDiscipline.PRACTICE -> R.color.practice_side_on
-                TDiscipline.LABORATORY -> R.color.lab_side_on
-                else -> R.color.other_side_on
+                val textViewTeacher = cardView.findViewById<TextView>(R.id.teacher)
+                // textViewTeacher.text = event.
+
+                val cardColor = when (event.disciplineType?.name) {
+                    TDiscipline.LECTURE -> R.color.lecture_side_on
+                    TDiscipline.PRACTICE -> R.color.practice_side_on
+                    TDiscipline.LABORATORY -> R.color.lab_side_on
+                    else -> R.color.other_side_on
+                }
+                cardView.setCardBackgroundColor(
+                    ContextCompat.getColor(
+                        requireContext(),
+                        cardColor
+                    )
+                )
+
+                val textContainer = cardView.findViewById<LinearLayout>(R.id.text_container)
+
+                val linearColor = when (event.disciplineType?.name) {
+                    TDiscipline.LECTURE -> R.color.lecture_main
+                    TDiscipline.PRACTICE -> R.color.practice_main
+                    TDiscipline.LABORATORY -> R.color.lab_main
+                    else -> R.color.other_main
+                }
+                textContainer.setBackgroundColor(
+                    ContextCompat.getColor(
+                        requireContext(),
+                        linearColor
+                    )
+                )
+
+                scheduleLayout.addView(cardView)
+
+                val constraints = ConstraintSet()
+                constraints.clone(scheduleLayout)
+
+                constraints.connect(
+                    cardView.id, ConstraintSet.START,
+                    timeColumn.id, ConstraintSet.END,
+                    10
+                )
+
+                constraints.connect(
+                    cardView.id, ConstraintSet.TOP,
+                    scheduleLayout.id, ConstraintSet.TOP,
+                    dpToPx(calculateStartPosition(startTimeFormatted))
+                )
+
+                constraints.connect(
+                    cardView.id, ConstraintSet.END,
+                    scheduleLayout.id, ConstraintSet.END,
+                    16
+                )
+
+                constraints.applyTo(scheduleLayout)
             }
-            cardView.setCardBackgroundColor(ContextCompat.getColor(requireContext(),
-                cardColor
-            ))
-
-            val textContainer = cardView.findViewById<LinearLayout>(R.id.text_container)
-
-            val linearColor = when (event.disciplineType?.name) {
-                TDiscipline.LECTURE -> R.color.lecture_main
-                TDiscipline.PRACTICE -> R.color.practice_main
-                TDiscipline.LABORATORY -> R.color.lab_main
-                else -> R.color.other_main
-            }
-            textContainer.setBackgroundColor(ContextCompat.getColor(requireContext(),
-                linearColor
-            ))
-
-            scheduleLayout.addView(cardView)
-
-            val constraints = ConstraintSet()
-            constraints.clone(scheduleLayout)
-
-            constraints.connect(
-                cardView.id, ConstraintSet.START,
-                timeColumn.id, ConstraintSet.END,
-                10
-            )
-
-            constraints.connect(
-                cardView.id, ConstraintSet.TOP,
-                scheduleLayout.id, ConstraintSet.TOP,
-                dpToPx(calculateStartPosition(event.startTime))
-            )
-
-            constraints.connect(
-                cardView.id, ConstraintSet.END,
-                scheduleLayout.id, ConstraintSet.END,
-                16
-            )
-
-            constraints.applyTo(scheduleLayout)
         }
     }
+
+/*    CoroutineScope(Dispatchers.IO).launch {
+        //обережено хард код:
+        scheduleViewModel.getEventInfo(event.id, week!!.toInt())
+    }
+
+    scheduleViewModel.getEventInfoResponse
+    .observe(viewLifecycleOwner) { eventInfoResponse ->
+        eventInfoResponse?.let {
+            when (eventInfoResponse) {
+                is BaseResponse.Success -> {
+                    val teachers = eventInfoResponse.data?.teachers ?: emptyList()
+                }
+
+                is BaseResponse.Error -> {
+                }
+
+                is BaseResponse.Loading -> {
+                }
+            }
+        }
+    }*/
 
     private fun dpToPx(dp: Int): Int {
         val scale = resources.displayMetrics.density
