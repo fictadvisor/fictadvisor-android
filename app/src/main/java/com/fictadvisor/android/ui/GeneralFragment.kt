5c643c05c243cf8f258478c52b0b0746f75543bd
vita133 <vitandrukhiv@gmail.com>
2024-04-29 18:29:43 +0300

    add ui for StudentProfileFragment

--- app/src/main/java/com/fictadvisor/android/ui/GeneralFragment.kt
+++ app/src/main/java/com/fictadvisor/android/ui/GeneralFragment.kt
@@ -1,45 +1,37 @@
 package com.fictadvisor.android.ui
 
 import android.os.Bundle
+import android.text.Editable
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.bumptech.glide.Glide
 import com.fictadvisor.android.R
+import com.fictadvisor.android.data.dto.OrdinaryStudentResponse
+import com.fictadvisor.android.databinding.FragmentGeneralBinding
 import com.fictadvisor.android.databinding.FragmentStudentProfileBinding
 import com.fictadvisor.android.utils.StorageUtil
 
 
-
-class StudentProfileFragment : Fragment() {
-    private lateinit var binding: FragmentStudentProfileBinding
+class GeneralFragment : Fragment() {
+    private lateinit var binding: FragmentGeneralBinding
     private lateinit var storageUtil: StorageUtil
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentStudentProfileBinding.inflate(inflater, container, false)
+    ): View? {
+        binding = FragmentGeneralBinding.inflate(inflater, container, false)
         storageUtil = StorageUtil(requireContext())
         val view = binding.root
+
         showUserData()
 
-        binding.securityTab.setOnClickListener {
-            Navigation.findNavController(view).navigate(R.id.action_studentProfileFragment_to_securityStudentProfileFragment)
+        binding.imageViewBack.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_generalFragment_to_studentProfileFragment)
         }
-
-        binding.buttonLogout.setOnClickListener {
-            storageUtil.clearAll()
-            activity?.finish()
-        }
-
-        binding.generalTab.setOnClickListener {
-            Navigation.findNavController(view).navigate(R.id.action_studentProfileFragment_to_generalFragment)
-        }
-
-
         return view
     }
 
@@ -47,10 +39,12 @@ class StudentProfileFragment : Fragment() {
         if (storageUtil.getTokens() != null) {
             val student = storageUtil.getOrdinaryStudentInfo()
             if (student != null) {
-                binding.name.text = String.format("%s %s", student.firstName, student.lastName)
-                binding.groupNumber.text = student.group.code
+                binding.editTextTextLastname.text = Editable.Factory.getInstance().newEditable(student.lastName)
+                binding.editTextTextName.text = Editable.Factory.getInstance().newEditable(student.firstName)
+                binding.editTextTextFathername.text = Editable.Factory.getInstance().newEditable(student.middleName)
                 Glide.with(this).load(student.avatar).into(binding.avatar)
             }
         }
     }
+
 }
